c4475f10903bd1eb7385482202e07c90746c1696
russellcachia <rusl.cachia@gmail.com>
2025-01-31 21:42:58 +0100

    Update Program.cs

--- ClockPatience/ClockPatience/Program.cs
+++ ClockPatience/ClockPatience/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            List<string> inputDecks = new List<string>();
+            string line;
+
+            // reading the input
+            while ((line = Console.ReadLine()) != "#")
+            {
+                inputDecks.Add(line);
+            }
+
+            // processing the deck
+            foreach (var deckInput in inputDecks)
+            {
+                List<Card> cards = new List<Card>();
+                string[] cardStrings = deckInput.Split(' ');
+
+                foreach (var cardString in cardStrings)
+                {
+                    string rank = cardString.Substring(0, 1);
+                    string suit = cardString.Substring(1, 1);
+                    cards.Add(new Card(rank, suit));
+                }
+
+                // creating and dealing the cards
+                Deck deck = new Deck();
+                deck.DealCards(cards);
+
+                // play the game and get result
+                var result
+            }
         }
     }
 }
