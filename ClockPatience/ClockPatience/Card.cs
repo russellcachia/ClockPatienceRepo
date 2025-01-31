daea9c3e646a63312a6924f9b2df45e8657cb778
russellcachia <rusl.cachia@gmail.com>
2025-01-31 20:35:53 +0100

    updated card.cs and deck.cs

--- ClockPatience/ClockPatience/Card.cs
+++ ClockPatience/ClockPatience/Card.cs
@@ -8,5 +8,41 @@ namespace ClockPatience
 {
     class Card
     {
+        public string Rank { get; set; }
+        public string Suit { get; set; }
+
+        public Card(string rank, string suit) 
+        {
+            Rank = rank;
+            Suit = suit;
+        }
+
+        //add method to convert card rank to numerical value (for placement)
+        public int GetPileIndex()
+        {
+            switch(Rank)
+            {
+                case "A": return 1; //ace
+                case "2": return 2;
+                case "3": return 3;
+                case "4": return 4;
+                case "5": return 5;
+                case "6": return 6;
+                case "7": return 7;
+                case "8": return 8;
+                case "9": return 9;
+                case "T": return 10; //ten
+                case "J": return 11; //jack
+                case "Q": return 12; //queen
+                case "K": return 13; //king
+
+                default: return 0;
+            }
+        }
+
+        public override string ToString() 
+        {
+            return $"{Rank}{Suit}";
+        }
     }
 }
--- ClockPatience/ClockPatience/Deck.cs
+++ ClockPatience/ClockPatience/Deck.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,33 @@ namespace ClockPatience
 {
     internal class Deck
     {
+        private Stack<Card>[] piles = new Stack<Card>[13]; // an array of 13 stacks, each holding Card objects called piles
+
+        public Deck()
+        {
+            // initializing all piles
+            for (int i = 0; i < 13; i++)
+            {
+                piles[i] = new Stack<Card>();
+            }
+        }
+
+        // dealing the cards to the piles
+        public void DealCards(List<Card> cards)
+        {
+            int pileIndex = 0;
+
+            foreach (Card card in cards)
+            {
+                piles[pileIndex].Push(card);
+                pileIndex = (pileIndex + 1) % 13; // div by 13 instead of piles.length to rotate through the piles
+            }
+        }
+
+        // returning the pile stack at the given index
+        public Stack<Card> GetPile(int index)
+        {
+            return piles[index];
+        }
     }
 }
